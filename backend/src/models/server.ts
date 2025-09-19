ad473a71db91161325b861fa99d5a6fa03fd99c9
GiselDiaz <gis_ale13@hotmail.com>
2025-09-19 17:07:03 -0600

    login

--- backend/src/models/server.ts
+++ backend/src/models/server.ts
@@ -1,7 +1,9 @@
 import express, {Application} from 'express'
+import routeUser from "../routes/user";
 import cors from 'cors'
 import path from 'path';
-
+import cookieParser from 'cookie-parser'
+import { verifyToken } from '../middlewares/auth';
 
 class Server {
 
@@ -25,6 +27,7 @@ class Server {
     }
 
     router(){
+          this.app.use(routeUser);
     }
 
 
@@ -32,8 +35,22 @@ class Server {
     midlewares(){
         //Parseo BOdy
         this.app.use(express.json())
-        this.app.use(cors())
+         this.app.use(cors({
+            origin: 'http://localhost:4200',
+            credentials: true
+        }))
+        this.app.use(cookieParser());
         this.app.use('/storage', express.static(path.join(process.cwd(), 'storage')));
+        this.app.use(function (req, res, next) {
+            const publicPaths = [
+                '/api/user/login',
+            ];
+            const isPublic = publicPaths.some(path => req.originalUrl.startsWith(path));
+            if (isPublic) {
+                return next(); 
+            }
+            return verifyToken(req, res, next); 
+        });
     }
 
     async DBconnetc(){
