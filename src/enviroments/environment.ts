f4afd544d5ded05dca0af587c554ab795b831e3e
MarSGlez <martin.sanchezg@hotmail.com>
2025-11-24 10:48:14 -0600

    add conections

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    appUrl: 'http://localhost:3006/'
+    //appUrl: 'http://localhost:3006/'
+    appUrl: 'https://bibliolex.gob.mx/repositorio/backend/'
 };
